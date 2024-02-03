2ce5d211c29b0100b2f7dfec9d3a7bf88c05dfec
khopadepriya <153990413+khopadepriya@users.noreply.github.com>
2024-02-03 13:22:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+// =========SEARCH========\\
+// const searchButton = document.getElementById('search-button'),
+// search = document.getElementById('search-close'),
+// searchContent = document.getElementById('search-content')
+// if(searchButton){
+//     searchButton.addEventListener('click', () =>{
+//         searchContent.classList.add('show-search')
+//     })
+// }
+// if(searchClose){
+//     searchClose.addEventListener('click', ()=>{
+//         searchContent.classList.remove('show-search')
+//     })
+// }
+
+// // =========Login========//
+
+// const loginButton = document.getElementById('login-button'),
+//        loginClose = document.getElementById('login-close'),
+//      loginContent = document.getElementById('login-content')
+// if(loginButton){
+//     loginButton.addEventListener('click', () =>{
+//         loginContent.classList.add('show-login')
+//     })
+// }
+// if(loginClose){
+//     loginClose.addEventListener('click', ()=>{
+//         loginContent.classList.remove('show-login')
+//     })
+// }
